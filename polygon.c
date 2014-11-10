5be5e297d42bd627ca84b19cdecd26a249d6f272
vskilet <victor.sene@utbm.fr>
2014-11-10 18:44:56 +0100

    Start create createPoint function in C

---
+++ polygon.c
@@ -0,0 +1,13 @@
+/** Author : Paul TREHIOU & Victor SENE
+ *  Date : November 2014
+ **/
+
+/**
+  * Function wich create a point with a specified abscisse and ordinate
+  * abscisse - real
+  * ordinate - real
+  * return a new point
+  */
+createPoint(float abscisse, float ordinate){
+	return EXIT_SUCCESS;
+}
