6205969806273ab7080203882a9585416cfcd302
vskilet <victor.sene@utbm.fr>
2014-11-10 18:26:49 +0100

    Add comment on header

--- polygon.h
+++ polygon.h
@@ -1,3 +1,12 @@
 /** Author : Paul TREHIOU & Victor SENE
  *  Date : November 2014
  **/
+
+ /**
+  * Function wich create a point with a specified abscisse and ordinate
+  * abscisse - real
+  * ordinate - real
+  * return a new point
+  */
+
+  
