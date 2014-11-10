65e079c747cd417fdfb198550197a1868258f63a
vskilet <victor.sene@utbm.fr>
2014-11-10 23:36:44 +0100

    Add comment for function addPoint

--- polygon.h
+++ polygon.h
@@ -53,6 +53,7 @@ Polygon createPolygon();
  * Function wich add a point at the end of an existing polygon
  * inpoly - Polygon
  * inpoint - Point
+ * newelem - new Point in the Polygon
  * return a new polygon
  */
 Polygon addPoint(Polygon inpoly, Point inpoint);
