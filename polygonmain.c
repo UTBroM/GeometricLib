facf0422c56fc82a3a6d9097c582228b54ff2846
Paul TREHIOU <paul.trehiou@utbm.fr>
2014-11-10 23:47:59 +0100

    Print the size of the polygon

--- polygonmain.c
+++ polygonmain.c
@@ -9,14 +9,16 @@ int main(int argc, char* argv[]) {
 	lol=addPoint(lol, createPoint(12.6,-5.3));
 	lol=addPoint(lol, createPoint(-4.1,456.123));
 
-	printf("\n\nx premier point : %f", lol->value.x);
-	printf("\ny premier point : %f\n\n", lol->value.y);
+	printf("\n\ntaille : %d", lol.size);
 
-	printf("\n\nx 2eme point : %f", lol->next->value.x);
-	printf("\ny 2eme point : %f\n\n", lol->next->value.y);
+	printf("\n\nx premier point : %f", lol.head->value.x);
+	printf("\ny premier point : %f\n\n", lol.head->value.y);
 
-	printf("\n\nx 3eme point : %f", lol->next->next->value.x);
-	printf("\ny 3eme point : %f\n\n", lol->next->next->value.y);
+	printf("\n\nx 2eme point : %f", lol.head->next->value.x);
+	printf("\ny 2eme point : %f\n\n", lol.head->next->value.y);
+
+	printf("\n\nx 3eme point : %f", lol.head->next->next->value.x);
+	printf("\ny 3eme point : %f\n\n", lol.head->next->next->value.y);
 
 	return EXIT_SUCCESS;
 
