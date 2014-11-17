0a4726f9ea4a6977f97d424e15a4fc74e850537a
Paul TREHIOU <paul.trehiou@utbm.fr>
2014-11-17 23:13:48 +0100

    You can enter the points to test for the segmentsCross

--- polygonmain.c
+++ polygonmain.c
@@ -6,6 +6,7 @@ int main(int argc, char* argv[]) {
 	Polygon lol;
 	Point test = createPoint(-1,0.5);
 	int i;
+	double x1=0, x2=0, x3=0, x4=0, y1=0, y2=0, y3=0, y4=0;
 	Intersection* testlol;
 
 	lol=createPolygon();
@@ -47,17 +48,39 @@ int main(int argc, char* argv[]) {
 	}
 
 
-	printf("\n\n==================================\n\nTest d'une intersection");
+	printf("\n==================================\n\nTest d'une intersection");
 
-	testlol = segmentsCross(createPoint(0,1), createPoint(0,-2), createPoint(-3,-1), createPoint(4,-1));
+	printf("\n\nEntrez les coordonées du point 1 :\n	x:");
+	scanf("%lf",&x1);
+	printf("\n	y:");
+	scanf("%lf",&y1);
+	printf("\n\nEntrez les coordonées du point 2 :\n	x:");
+	scanf("%lf",&x2);
+	printf("\n	y:");
+	scanf("%lf",&y2);
+	printf("\n\nEntrez les coordonées du point 3 :\n	x:");
+	scanf("%lf",&x3);
+	printf("\n	y:");
+	scanf("%lf",&y3);
+	printf("\n\nEntrez les coordonées du point 4 :\n	x:");
+	scanf("%lf",&x4);
+	printf("\n	y:");
+	scanf("%lf",&y4);
+
+	testlol = segmentsCross(createPoint(x1,y1), createPoint(x2,y2), createPoint(x3,y3), createPoint(x4,y4));
 
 	if(testlol != NULL){
 
 		printf("\n\nL'intersection est : \n\n	x: %f\n	y: %f\n\n", testlol->value.x, testlol->value.y);
 
 	}
+	else{
 
+		printf("\n\nIl n'y a pas d'intersection\n\n");
 
+	}
+
+	free(testlol);
 
 	return EXIT_SUCCESS;
 
