1155b0a6afeb878b29c877eef46179a06a19fc65
Victor SENE <victor.sene@utbm.fr>
2014-12-31 11:23:07 +0100

    declaration of insertTail
    sorry for this stupid forgetting

--- polygon.h
+++ polygon.h
@@ -134,6 +134,13 @@ double max(double a, double b);
 Polygon removeTail (Polygon inpoly);
 
 /**
+ * Function wich insert a polygon at the end of outpoly
+ * outpoly, tailpoly - Polygon
+ * Return outpoly 
+ **/
+Polygon insertTail (Polygon outpoly, tailpoly);
+
+/**
  * Function wich return the convex hull of a polygon
  * inpoly - Polygon
  * Return outpoly
